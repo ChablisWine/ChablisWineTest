2ea7d1dbc247c0b14101e56031459482213989e9
ChablisWine <1248843475@qq.com>
2021-04-25 03:34:47 +0800

    Add HelloChablis to GitHub

---
+++ ChablisTest/src/main/java/com/chablis/HelloChablis.java
@@ -0,0 +1,11 @@
+package com.chablis;
+
+/**
+ * @author Chablis
+ * @create 2021-04-25 3:30
+ */
+public class HelloChablis {
+    public static void main(String[] args) {
+        System.out.println("Add HelloChablis to GitHub!");
+    }
+}
